6f86b4031682249a90c5b8163582c0479e6e0be6
PCS\jackchuang <jackchuang@taiwanmobile.com>
2024-07-23 14:59:16 +0800

    jack 2024/07/23 006

--- exec_test.bat
+++ exec_test.bat
@@ -1,5 +1,6 @@
 C:\Users\jackchuang\AppData\Local\Programs\Python\Python312\python.exe --version
 
 C:\Users\jackchuang\AppData\Local\Programs\Python\Python312\Scripts\pip install -r requirements.txt
+
 pytest -n2
 
